4d6eb6f6b12aa94dfd0477c52d4b06b42e3fee3f
Deepa <k.deepa050903@gmail.com>
2026-04-10 17:16:27 +0530

    space added

--- p2.py
+++ p2.py
@@ -4,5 +4,5 @@ for line in range(1,num+1):
         if line==1 or line==num or line == col or col+line == num+1 :
             print('*',end=' ')
         else:
-            print(' ',end ='  ')
+            print(' ',end ='   ')
     print()
